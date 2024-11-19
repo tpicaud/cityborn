96e5a145e7add7dce47332fb8bedef3b674886b9
tpicaud <tom.picaud@hotmail.com>
2024-11-19 14:52:36 +0100

    add total points

--- src/utils/calculateScore.ts
+++ src/utils/calculateScore.ts
@@ -1,6 +1,12 @@
+import { PlayerResults } from "@/types/Results";
+
 // Calculates points based on distance
 const calculatePoints = (distance: number) => {
     return Math.max(0, Math.round(1000 * Math.exp(-0.0006 * distance)));
 }
 
-export { calculatePoints };
+const calculateTotalPoints = (results: PlayerResults) => {
+    return results.results.reduce((acc, result) => acc + result.points, 0);
+}
+
+export { calculatePoints, calculateTotalPoints };
