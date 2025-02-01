efa1d4fe61cd9a38bff862cd23b64e3a0242d439
tpicaud <tom.picaud@hotmail.com>
2025-02-01 21:14:52 +0100

    continue refactoring

---
+++ application/src/app/api/game/route.ts
@@ -0,0 +1,133 @@
+import { NextResponse } from "next/server";
+import { MongoClient } from "mongodb";
+import { GameStatus } from "@/enums/GameStatus";
+import Game from "@/types/Game";
+import GameConfig from "@/types/GameConfig";
+import GuessObject from "@/types/GuessObject";
+import { Categories } from "@/enums/Categories";
+import { adjectives, animals, colors, uniqueNamesGenerator } from "unique-names-generator";
+
+// Configuration de la connexion MongoDB
+const uri = process.env.NEXT_PUBLIC_MONGODB_URI || "";
+const client = new MongoClient(uri);
+
+// Nom de des bases de données et des collections
+const gameDB = process.env.NEXT_PUBLIC_GAMES_DB || '';
+const gameCollection = process.env.NEXT_PUBLIC_GAMES_COLLECTION || '';
+const celebritiesDB = process.env.NEXT_PUBLIC_CELEBRITIES_DB || ''
+const celebritiesCollection = process.env.NEXT_PUBLIC_CELEBRITIES_COLLECTION || ''
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        const { hostID, gameConfig } = body;
+
+        if (!hostID || !gameConfig) {
+            return NextResponse.json(
+                { message: "hostID et config sont requis." },
+                { status: 400 }
+            ); 
+        }
+
+        // Fetch guessObjects
+        const guessObjects = await fetchGuessObjects(gameConfig);
+
+        // Connexion à la base de données
+        await client.connect();
+        const db = client.db(gameDB);
+        const collection = db.collection(gameCollection);
+
+        // fetch
+
+        // Création de la nouvelle game
+        const newGame: Game = {
+            id: uniqueNamesGenerator({
+                dictionaries: [adjectives, colors, animals],
+                separator: '-',
+                length: 3
+            }),
+            hostID,
+            status: GameStatus.LOBBY, // statut initial du jeu
+            gameConfig,
+            players: [hostID],
+            guessObjects: guessObjects,
+            currentRound: undefined,
+        };
+
+        // Insérer la nouvelle game dans la base de données
+        await collection.insertOne(newGame);
+
+        return NextResponse.json(newGame, { status: 201 });
+    } catch (error) {
+        console.error("Erreur lors de la création de la game:", error);
+        return NextResponse.json(
+            { message: "Erreur lors de la création de la game." },
+            { status: 500 }
+        );
+    } finally {
+        // Fermer la connexion MongoDB
+        await client.close();
+    }
+}
+
+async function fetchGuessObjects(gameConfig: GameConfig): Promise<GuessObject[]> {
+    try {
+        // Connexion à la base de données
+        await client.connect();
+        const db = client.db(celebritiesDB);
+        const collection = db.collection(celebritiesCollection);
+
+        // Construire la pipeline d'agrégation
+        const pipeline: any[] = [];
+
+        // Filtrer par catégories si nécessaire
+        if (gameConfig.categories.length > 0 || gameConfig.categories.includes(Categories.TOUTES)) {
+            pipeline.push({ $match: { category: { $in: gameConfig.categories } } });
+        }
+
+        // Sélectionner un nombre aléatoire d'objets
+        pipeline.push({ $sample: { size: gameConfig.nbOfObjects } });
+
+        // Exécuter l'agrégation
+        const randomCelebrities = await collection.aggregate(pipeline).toArray();
+
+        // Map response in guessObjects
+        const guessObjects: GuessObject[] = randomCelebrities
+            .filter((doc) =>
+                doc.name &&
+                doc.category &&
+                doc.description &&
+                doc.short_description &&
+                doc.image &&
+                doc.answer?.place_name &&
+                doc.answer?.coordinates?.type &&
+                doc.answer?.coordinates?.value
+            )
+            .map((doc) => ({
+                name: doc.name,
+                category: doc.category,
+                description: doc.description,
+                short_description: doc.short_description,
+                image: doc.image,
+                answer: {
+                    place_name: doc.answer.place_name,
+                    coordinates: {
+                        type: doc.answer.coordinates.type,
+                        value: doc.answer.coordinates.value,
+                    },
+                },
+            }));
+
+
+
+        // Retourner les résultats
+        return guessObjects;
+    } catch (error) {
+        console.error("Erreur lors de la récupération des célébrités :", error);
+        throw new Error("Erreur lors de la récupération des célébrités.");
+    } finally {
+        // Fermer la connexion MongoDB
+        await client.close();
+    }
+}
+
