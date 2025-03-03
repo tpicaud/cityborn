00a43c8324fe556f3bb05ab054535a7c7c219de4
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2025-03-03 19:33:17 +0100

    add swagger

---
+++ application/src/app/api/docs/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server";
+import swaggerDefinition from "@/lib/swagger";
+
+export function GET() {
+    return NextResponse.json(swaggerDefinition);
+}
---
+++ application/src/app/docs/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+import { useEffect, useState } from "react";
+
+export default function ApiDocsPage() {
+    const [swaggerSpec, setSwaggerSpec] = useState<any>(null);
+
+    useEffect(() => {
+        const fetchSwaggerSpec = async () => {
+            const response = await fetch("/api/docs");
+            const data = await response.json();
+            setSwaggerSpec(data);
+        };
+
+        fetchSwaggerSpec();
+    }, []);
+
+    if (!swaggerSpec) return <p>Chargement de la documentation...</p>;
+
+    return <SwaggerUI spec={swaggerSpec} />;
+}
