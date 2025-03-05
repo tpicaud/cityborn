70cb3dad632ad183fe2608017ef2f6b90c40a0cf
tpicaud <tom.picaud@hotmail.com>
2025-03-05 09:09:03 +0100

    move connectToDatabase

--- application/src/app/api/game/[id]/route.ts
+++ application/src/app/api/game/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectToDatabase } from "../../utils";
+import { connectToDatabase } from "@/utils/connectToDatabase";
 
 export async function GET(
   request: Request,
