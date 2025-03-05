caac713bac020c1a9a884e9b85f1ea1ae88855ae
tpicaud <tom.picaud@hotmail.com>
2025-03-05 12:29:47 +0100

    fix and add endpoint for joining, handle nextround and start game

--- application/src/app/api/game/join/route.ts
+++ application/src/app/api/game/join/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/utils/connectToDatabase";
+import Game from "@/types/Game";
+import { GameStatus } from "@/enums/GameStatus";
 
 export async function PUT(request: Request) {
     try {
@@ -12,11 +14,38 @@ export async function PUT(request: Request) {
         const db = client.db(process.env.NEXT_PUBLIC_GAMES_DB);
         const collection = db.collection(process.env.NEXT_PUBLIC_GAMES_COLLECTION!);
 
+        // Récupération du jeu dans la base de données
+        const game = (await collection.findOne({ id: gameID })) as Game | null;
+
+        if (!game) {
+            return NextResponse.json({ message: "Partie introuvable." }, { status: 404 });
+        }
+
+        // Check si l'id du joueur est déjà dans la partie
+        const playerExists = game.players.some(player => player.id === playerID);
+
+        if (playerExists) {
+            return NextResponse.json({ message: "Le joueur est déjà dans la partie." }, { status: 400 });
+        }
+
+        // Check si la partie est déjà lancé
+        if (game.status != GameStatus.LOBBY) {
+            return NextResponse.json({ message: "La partie est déjà lancée." }, { status: 400 });
+        }
+
+        // Créer un nouveau joueur
         const newPlayer: any = { id: playerID, results: [], connected: true };
+
+        // Si la liste des players est vide, le nouveau joueur devient host
+        let updateFields: any = { $push: { players: newPlayer } };
+
+        if (game.players.length === 0) {
+            updateFields.$set = { hostID: playerID }; // Mettre à jour le hostID si players est vide
+        }
 
         await collection.updateOne(
             { id: gameID },
-            { $push: { players: newPlayer } }
+            updateFields
         );
 
         return NextResponse.json({ message: "Joueur ajouté avec succès." }, { status: 200 });
