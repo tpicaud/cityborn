254cde9aa336773c19a04bffff1fdd0c578acd0e
tpicaud <tom.picaud@hotmail.com>
2025-04-01 17:14:14 +0200

    fix hostID when reconnection

--- backend/gameFunctions.ts
+++ backend/gameFunctions.ts
@@ -301,6 +301,9 @@ export async function reconnect(socket: Socket, gameID: string, playerID: string
             throw new Error("Joueur introuvable dans la partie")
         }
 
+        if (game.hostID === '') {
+            game.hostID = playerID
+        } 
         game.players = game.players.map(
             player => player.id === playerID ?
                 {
