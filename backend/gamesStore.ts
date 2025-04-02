c57811e330e90564c84a9a4f3715c4522b7813e2
tpicaud <tom.picaud@etudiant.univ-rennes1.fr>
2025-04-02 21:42:05 +0200

    minor changes

--- backend/gamesStore.ts
+++ backend/gamesStore.ts
@@ -2,7 +2,7 @@
 let games: any[] = [];
 
 // Seuil d'inactivité d'une partie
-const INACTIVITY_LIMIT = 1 * 60 * 1000; // 5 minutes en millisecondes
+const INACTIVITY_LIMIT = 5 * 60 * 1000; // 5 minutes en millisecondes
 
 // Nettoyage des parties inactives
 setInterval(() => {
