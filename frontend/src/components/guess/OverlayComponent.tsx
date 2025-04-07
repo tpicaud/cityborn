2270dd6b6ae60e81a0267d455c59db7d1e0d031c
tpicaud <tom.picaud@hotmail.com>
2025-04-07 15:36:46 +0200

    fix build errors

--- frontend/src/components/guess/OverlayComponent.tsx
+++ frontend/src/components/guess/OverlayComponent.tsx
@@ -1,5 +1,5 @@
 import Guess from "@/types/Guess";
-import { Box, Button, Chip } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import GuessObjectComponent from "./GuessObjectComponent";
 import TimerComponent from "./TimerComponent";
 import Round from "@/types/Round";
