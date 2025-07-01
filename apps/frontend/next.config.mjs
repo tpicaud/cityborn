0c8ca8e28a2c42f3ead0ef639d9640df33fb4bf1
tpicaud <tom.picaud@hotmail.com>
2025-07-01 13:48:38 +0200

    accept cors from backend

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+
+    // CORS config
+    async headers() {
+        const allowedOrigin = process.env.NEXT_PUBLIC_WEBSOCKET_URL || "*";
+
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: allowedOrigin,
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET, POST, PUT, DELETE, OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type, Authorization",
+                    },
+                ],
+            },
+        ];
+    },
 };
+
 
 export default nextConfig;
