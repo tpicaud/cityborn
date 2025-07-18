1af3ae4f458043cccf41daf13199e094b1e6a8d2
tpicaud <tom.picaud@hotmail.com>
2025-07-18 16:59:33 +0200

    finish join example

--- apps/backend/src/session/session.gateway.ts
+++ apps/backend/src/session/session.gateway.ts
@@ -1,21 +1,37 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { SessionService } from './session.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class SessionGateway {
 
-  constructor(private readonly sessionService: SessionService) { }
+	constructor(private readonly sessionService: SessionService) { }
 
-  @SubscribeMessage('session:join')
-  handleMessage(
-    @ConnectedSocket() socket: Socket,
-    @MessageBody('sessionID') sessionID: string,
-    @MessageBody('playerID') playerID: string
-  ): string {
-    console.log(socket.id)
-    console.log(sessionID);
-    console.log(playerID)
-    return 'Hello world!';
-  }
+	@WebSocketServer()
+	io: Server;
+
+	@SubscribeMessage('session:join')
+	async handleMessage(
+		@ConnectedSocket() socket: Socket,
+		@MessageBody('sessionID') sessionID: string,
+		@MessageBody('playerID') playerID: string
+	): Promise<{ success: boolean; error?: string; }> {
+		try {
+			if (!sessionID || !playerID) throw new Error("Paramètres invalides : sessionID et playerID sont requis.");
+
+			const session = await this.sessionService.join(socket.id, sessionID, playerID);
+
+			await socket.join(sessionID)
+			this.io.to(sessionID).emit('session:update', session);
+
+			console.log(`${playerID} a rejoint la session ${sessionID}`);
+			return { success: true };
+		} catch (error) {
+			console.error("Erreur lors de la tentative de rejoindre la partie :", error);
+			return {
+				success: false,
+				error: error.message || "Une erreur inconnue s'est produite."
+			};
+		}
+	}
 }
