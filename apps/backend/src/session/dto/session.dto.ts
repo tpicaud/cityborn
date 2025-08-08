774e4a20e110dff507af4370f51549bf3218596b
tpicaud <tom.picaud@hotmail.com>
2025-08-08 12:11:14 +0200

    add dto and dto response

---
+++ apps/backend/src/session/dto/session.dto.ts
@@ -0,0 +1,31 @@
+import { GameConfig, GameMode, Session, SessionStatus } from "@cityborn/types";
+import { Type } from "class-transformer";
+import { IsArray, IsEnum, IsOptional, IsString, ValidateNested } from "class-validator";
+import { GameConfigDto } from "src/game/dto/game.dto";
+import { PlayerDto } from "src/player/dto/player.dto";
+
+export class SessionDto implements Session {
+  @IsString()
+  id: string;
+
+  @IsString()
+  hostID: string;
+
+  @IsEnum(GameMode)
+  mode: GameMode;
+
+  @IsEnum(SessionStatus)
+  status: SessionStatus;
+
+  @ValidateNested()
+  @Type(() => GameConfigDto)
+  gameConfig: GameConfigDto;
+
+  @IsArray()
+  @Type(() => PlayerDto)
+  players: PlayerDto[];
+
+  @IsOptional()
+  @IsString()
+  currentGameId?: string;
+}
