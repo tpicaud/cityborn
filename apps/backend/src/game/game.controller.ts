2ccc8e3cbfb4aefd40dc9020e2cbaeb25163b232
tpicaud <tom.picaud@hotmail.com>
2025-08-08 17:58:41 +0200

    add auth service and ignore sqlite db

--- apps/backend/src/game/game.controller.ts
+++ apps/backend/src/game/game.controller.ts
@@ -1,23 +1,24 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { GameService } from './game.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { GameResponseDto } from './dto/game.response.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('game')
 export class GameController {
-    constructor(private readonly gameService: GameService) {}
+    constructor(private readonly gameService: GameService) { }
 
-        @Post()
-        async createGame(@Body() createGameDto: CreateGameDto): Promise<GameResponseDto> {
-            return {
-                game: await this.gameService.create(createGameDto)
-            }
+    @Post()
+    async createGame(@Body() createGameDto: CreateGameDto): Promise<GameResponseDto> {
+        return {
+            game: await this.gameService.create(createGameDto)
         }
-    
-        @Get()
-        async getGame(@Query('gameId') gameId: string): Promise<GameResponseDto> {
-            return {
-                game: await this.gameService.get(gameId)
-            }
+    }
+
+    @Get()
+    async getGame(@Query('gameId') gameId: string): Promise<GameResponseDto> {
+        return {
+            game: await this.gameService.get(gameId)
         }
+    }
 }
