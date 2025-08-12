340c6a8c0534ccb93c3466df5166e28b96b0f491
tpicaud <tom.picaud@hotmail.com>
2025-08-13 00:14:20 +0200

    display auth

--- apps/frontend/src/components/HomeComponent.tsx
+++ apps/frontend/src/components/HomeComponent.tsx
@@ -1,19 +1,75 @@
 'use client';
 
-import { useAuth } from "@/contexts/AuthContext";
-import { AuthComponent } from "./auth/AuthComponent";
 import MenuComponent from "./MenuComponent";
+import { useEffect, useState } from "react";
+import { SignInComponent } from "./auth/SignInComponent";
+import { SignUpComponent } from "./auth/SignUpComponent";
+import dynamic from "next/dynamic";
+import { Box, IconButton } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useAuth } from "@/contexts/AuthContext";
+
+const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
 
 export default function HomeComponent() {
+
     const { user } = useAuth();
+    const [state, setState] = useState<'menu' | 'sign-in' | 'sign-up'>('menu');
+
+useEffect(() => {
+    if (user) {
+        setState('menu');
+    }
+}, [user])
+
+    let content;
+
+    switch (state) {
+        case 'sign-in':
+            content = <SignInComponent />;
+            break;
+
+        case 'sign-up':
+            content = <SignUpComponent />
+            break;
+
+        case 'menu':
+            content = <MenuComponent setState={setState} />
+            break;
+
+        default:
+            content = <MenuComponent setState={setState} />
+    }
 
     return (
-        <div>
-            {user ? (
-                <MenuComponent />
-            ) : (
-                <AuthComponent />
-            )}
+        <div className="relative h-screen">
+            <div className="absolute inset-0">
+                <MapContainer center={[0, 0]} zoom={3} zoomControl={false} className="h-full w-full z-0">
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                </MapContainer>
+                <div className="absolute inset-0 bg-black opacity-60 z-10 pointer-events-none"></div>
+            </div>
+
+            <div className="relative z-10 flex flex-col items-center justify-center h-full p-4 bg-transparent pointer-events-none">
+                <Box className="relative flex flex-col items-center p-6 bg-slate-100 shadow-xl rounded-2xl max-w-[85%] pointer-events-auto">
+                    <IconButton
+                        onClick={() => setState('menu')}
+                        sx={{
+                            position: "absolute",
+                            top: 6,
+                            left: 6,
+                            visibility: state === 'menu' ? "hidden" : "visible"
+                        }}
+                    >
+                        <ArrowBackIcon />
+                    </IconButton>
+                    {content}
+                </Box>
+            </div>
         </div>
     )
 }
--- apps/frontend/src/components/MenuComponent.tsx
+++ apps/frontend/src/components/MenuComponent.tsx
@@ -1,18 +1,18 @@
 'use client'
 
-import dynamic from 'next/dynamic';
-import { Box, Button, Typography } from "@mui/material";
+import { Alert, Box, Button, Snackbar, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import 'leaflet/dist/leaflet.css';
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { GameMode } from '@cityborn/types';
 import * as ApiServiceClient from '@/services/ApiServiceClient';
 import { useAuth } from '@/contexts/AuthContext';
-const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
-const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
 
-
-export default function MenuComponent() {
+export default function MenuComponent({
+    setState,
+}: {
+    setState: Dispatch<SetStateAction<"menu" | "sign-in" | "sign-up">>;
+}) {
 
     const router = useRouter();
 
@@ -21,14 +21,19 @@ export default function MenuComponent() {
     const [code, setCode] = useState<string>('')
     const [joinErrorMessage, setJoinErrorMessage] = useState<string>();
     const [loadingJoin, setLoadingJoin] = useState(false);
+    const [openAlert, setOpenAlert] = useState(false);
 
     const handleSoloPlay = () => {
         router.push(`/session/solo`);
     };
 
     const handleMultiPlay = async () => {
-        const session = await ApiServiceClient.createSession(GameMode.MULTI);
-        router.push(`/session/multi/${session.id}`)
+        if (!user) {
+            setOpenAlert(true);
+        } else {
+            const session = await ApiServiceClient.createSession(GameMode.MULTI);
+            router.push(`/session/multi/${session.id}`);
+        }
     }
 
     const handleJoin = async () => {
@@ -44,27 +49,47 @@ export default function MenuComponent() {
         } catch {
             setJoinErrorMessage('Erreur lors de la connexion à la partie');
         }
+    }
 
-}
-
-return (
-    <div className="relative h-screen">
-        <div className="absolute inset-0">
-            <MapContainer center={[0, 0]} zoom={3} zoomControl={false} className="h-full w-full z-0">
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </MapContainer>
-            <div className="absolute inset-0 bg-black opacity-60 z-10 pointer-events-none"></div>
-        </div>
-
-        <div className="relative z-10 flex flex-col items-center justify-center h-full p-4 bg-transparent pointer-events-none">
-            <Box className="flex flex-col items-center gap-4 p-6 bg-slate-100 shadow-xl rounded-2xl max-w-[85%] pointer-events-auto">
-                <img src="/cityborn_transparent2.png" alt="Logo" className='mb-2 max-h-32 md:max-h-48' />
+    return (
+        <div className='flex flex-col items-center gap-5'>
+            <div className='flex flex-col gap-1 items-center w-full'>
+                <img src="/cityborn_transparent2.png" alt="Logo" className='mb-2 max-24 md:max-h-32' />
                 <p className="text-base md:text-lg text-center ">Trouvez le lieu de naissance des personnalités</p>
+            </div>
 
-                <div className="flex flex-row gap-2 items-center w-full mt-4">
+            {user ? (
+                <Typography variant="h5">
+                    Bienvenue <b>{user.username}</b> !
+                </Typography>
+            ) : (
+                <div className="flex flex-row gap-2 items-center justify-center w-full">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className="bg-green-500 hover:bg-green-600 text-white px-6 w-full rounded"
+                        onClick={async () => {
+                            setState('sign-in');
+                        }}
+                    >
+                        <p className='px-3'>Se connecter</p>
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className="bg-green-500 hover:bg-green-600 text-white px-6 w-full rounded"
+                        onClick={async () => {
+                            setState('sign-up');
+                        }}
+                    >
+                        <p className='px-3'>S'inscrire</p>
+                    </Button>
+                </div>
+            )}
+
+            <div className="flex flex-col gap-2 items-center justify-center w-full">
+
+                <div className="flex flex-row gap-2 items-center justify-center w-full">
                     <input
                         type="text"
                         placeholder="Code"
@@ -113,8 +138,22 @@ return (
                         {joinErrorMessage}
                     </Typography>
                 )}
-            </Box>
-        </div>
-    </div>
-);
+            </div>
+
+            <Snackbar
+                open={openAlert}
+                autoHideDuration={3000}
+                onClose={() => setOpenAlert(false)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            >
+                <Alert
+                    onClose={() => setOpenAlert(false)}
+                    severity="warning"
+                    variant="filled"
+                >
+                    Vous devez être connecté pour jouer en mode multi !
+                </Alert>
+            </Snackbar>
+        </div >
+    );
 }
