a0175895b14ada91eb8ce393895914f19ec45720
tpicaud <tom.picaud@hotmail.com>
2025-08-13 15:21:25 +0200

    better menu

--- apps/frontend/src/components/HomeComponent.tsx
+++ apps/frontend/src/components/HomeComponent.tsx
@@ -58,8 +58,8 @@ export default function HomeComponent() {
             </div>
 
             <div className="relative z-10 flex flex-col items-center justify-center h-full p-4 bg-transparent pointer-events-none">
-                <Box className="relative flex flex-col items-center p-6 bg-slate-100 shadow-xl rounded-2xl max-w-[85%] pointer-events-auto">
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col h-[95%] w-[95%]">
+                <Box className="relative flex flex-col p-6 bg-slate-100 shadow-xl rounded-2xl max-w-[85%] pointer-events-auto">
+                    <div className="absolute self-center top-2 flex flex-col h-[15%] w-[95%]">
                         <div className="flex flex-row items-start justify-between w-full">
                             <IconButton
                                 onClick={() => setState('menu')}
