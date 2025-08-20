40369abb89375f87c13b39f217230a953d03b22b
tpicaud <tom.picaud@hotmail.com>
2025-08-20 18:38:06 +0200

    finish refresh

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -21,6 +21,12 @@ async function bootstrap() {
   await redisIoAdapter.connectToRedis();
   app.useWebSocketAdapter(redisIoAdapter);
 
+  // Logger simple de toutes les requêtes
+  // app.use((req, res, next) => {
+  //   console.log(`${req.method} ${req.url}`);
+  //   next();
+  // });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
