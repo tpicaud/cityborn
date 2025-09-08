7feb727be5e40ee88ea8d68542b88622727df380
tpicaud <tom.picaud@hotmail.com>
2025-09-08 15:54:35 +0200

    clean up some logs and errors

--- apps/backend/src/redis/redis.adapter.ts
+++ apps/backend/src/redis/redis.adapter.ts
@@ -1,10 +1,14 @@
 // redis.adapter.ts
+import { Logger } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { createClient } from 'redis';
 import { ServerOptions } from 'socket.io';
 
 export class RedisIoAdapter extends IoAdapter {
+
+  private readonly logger = new Logger(RedisIoAdapter.name);
+
   private adapterConstructor: ReturnType<typeof createAdapter>;
 
   async connectToRedis(): Promise<void> {
@@ -13,6 +17,13 @@ export class RedisIoAdapter extends IoAdapter {
 
     await pubClient.connect();
     await subClient.connect();
+
+    pubClient.on('error', (err) => {
+      this.logger.error('Redis Pub Error:', err);
+    });
+    subClient.on('error', (err) => {
+      this.logger.error('Redis Sub Error:', err);
+    });
 
     this.adapterConstructor = createAdapter(pubClient, subClient);
   }
