57722461f1974779a98534caa95b6c659075698c
tpicaud <tom.picaud@hotmail.com>
2025-09-11 16:35:28 +0200

    force websocket transport

--- apps/frontend/src/lib/socket.ts
+++ apps/frontend/src/lib/socket.ts
@@ -15,6 +15,7 @@ declare global {
 export const getSocket = (): Socket => {
     if (!socket) {
         socket = io(WEBSOCKET_URL, {
+            transports: ['websocket'],
             withCredentials: true,
         });
     }
