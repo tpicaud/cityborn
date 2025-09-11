bb90537e47919de6a5e5419e72c6f52d5974f3f8
tpicaud <tom.picaud@hotmail.com>
2025-09-11 13:32:31 +0200

    try som fixes

--- apps/backend/src/auth/auth.gateway.ts
+++
@@ -1,5 +0,0 @@
-import { JwtService } from "@nestjs/jwt";
-import { Socket } from "socket.io";
-import { getJwtConstants } from "src/auth/constants";
-import { extractAccessTokenFromWsClient } from "./utils";
-import { ConfigService } from "@nestjs/config";
