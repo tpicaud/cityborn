d595ae2fd274ab44b923dc89620b9481461c6b95
tpicaud <tom.picaud@hotmail.com>
2025-09-11 16:41:25 +0200

    remove logs

--- apps/backend/src/auth/utils.ts
+++ apps/backend/src/auth/utils.ts
@@ -9,11 +9,9 @@ export function extractTokenFromHTTPHeader(request: Request): string | undefined
 export function extractAccessTokenFromWsClient(client?: any): string | undefined {
 
     const cookies = client.handshake.headers.cookie;
-    console.log(cookies);
     if (!cookies) return undefined;
 
     const parsedCookies = cookie.parse(cookies);
-    console.log(parsedCookies)
 
     return parsedCookies['access_token'];
 }
