b5edd00d873f5785d7820269945fc2ecf2a88265
tpicaud <tom.picaud@hotmail.com>
2025-10-01 10:22:01 +0200

    remove logs in prod

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
     reactStrictMode: false,
 
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production'
+    },
+
     // CORS config
     async headers() {
         return [
