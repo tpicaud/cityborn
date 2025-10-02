d333e65e9b3b41db119101e7b11761adfa5da4b0
tpicaud <tom.picaud@hotmail.com>
2025-10-02 21:10:49 +0200

    setup button variant

--- apps/frontend/src/features/menu/menu.tsx
+++ apps/frontend/src/features/menu/menu.tsx
@@ -2,8 +2,8 @@ import Button from "@/components/ui/buttons/Button"
 
 export const Menu = () => {
     return (
-        <Button className="bg-gradient-primary">
-            <p className="font-heading shadow-primary font-bold text-neutral-400">Jouer</p>
+        <Button variant="primary">
+            <p className="font-heading font-bold text-neutral-400">Jouer</p>
         </Button>
     )
 }
