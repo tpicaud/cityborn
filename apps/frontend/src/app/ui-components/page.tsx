23aa94719bf301d166c792f0386d58cacaac25e6
tpicaud <tom.picaud@hotmail.com>
2025-10-08 11:24:25 +0200

    finish menu

--- apps/frontend/src/app/ui-components/page.tsx
+++ apps/frontend/src/app/ui-components/page.tsx
@@ -1,116 +1,186 @@
 'use client';
 
 import Button from "@/components/ui/buttons/Button"
+import Card from "@/components/ui/cards/card";
+import { WordCarousel } from "@/features/menu/components/wordCarousel";
 
 export default function UiComponentsPage() {
     return (
-        <section className="min-h-screen h-full p-6">
-            <div className="flex flex-col items-center gap-3">
-                <h1 className="text-2xl p-1">Buttons</h1>
+        <section className="min-h-screen p-4 overflow-auto text-black">
+            <div className="flex flex-col items-center gap-10 overflow-auto p-4">
+                <div className="flex flex-col items-center gap-3">
+                    <h1 className="text-2xl p-1">Buttons</h1>
 
-                {/* Variants */}
-                <div className="grid grid-rows-3 grid-cols-3 border border-gray-300">
+                    {/* Variants */}
+                    <div className="grid grid-rows-3 grid-cols-3 border border-gray-300">
 
-                    {/* Header */}
-                    <div className="grid place-items-center p-6 border border-gray-300"></div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        Default
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        Disabled
+                        {/* Header */}
+                        <div className="grid place-items-center p-6 border border-gray-300"></div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Default
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Disabled
+                        </div>
+
+                        {/* 1 row */}
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Primary
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button variant="primary" size="md">
+                                LABEL
+                            </Button>
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button variant="primary" size="md" disabled={true}>
+                                LABEL
+                            </Button>
+                        </div>
+
+                        {/* 2 row */}
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Secondary
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button variant="secondary" size="md">
+                                LABEL
+                            </Button>
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button variant="secondary" size="md" disabled={true}>
+                                LABEL
+                            </Button>
+                        </div>
+
+                        {/* 3 row */}
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Accent
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button variant="accent" size="md">
+                                LABEL
+                            </Button>
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button variant="accent" size="md" disabled={true}>
+                                LABEL
+                            </Button>
+                        </div>
                     </div>
 
-                    {/* 1 row */}
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        Primary
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button variant="primary" size="md">
-                            LABEL
-                        </Button>
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button variant="primary" size="md" disabled={true}>
-                            LABEL
-                        </Button>
-                    </div>
-
-                    {/* 2 row */}
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        Secondary
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button variant="secondary" size="md">
-                            LABEL
-                        </Button>
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button variant="secondary" size="md" disabled={true}>
-                            LABEL
-                        </Button>
+                    {/* Sizes */}
+                    <div className="grid grid-rows-2 grid-cols-3 border border-gray-300">
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Small
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Medium
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Large
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button variant="primary" size="sm">
+                                LABEL
+                            </Button>
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button variant="primary" size="md">
+                                LABEL
+                            </Button>
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button variant="primary" size="lg">
+                                LABEL
+                            </Button>
+                        </div>
                     </div>
 
-                    {/* 3 row */}
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        Accent
+                    {/* Loading */}
+                    <div className="grid grid-rows-1 grid-cols-2">
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Loading button
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Button
+                                variant="primary"
+                                size="lg"
+                                onClick={async () => {
+                                    await new Promise(res => setTimeout(res, 2000));
+                                    console.log('loader finished');
+                                }}>
+                                LABEL
+                            </Button>
+                        </div>
                     </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button variant="accent" size="md">
-                            LABEL
-                        </Button>
+                </div>
+                <div className="flex flex-col items-center gap-3">
+                    <h1 className="text-2xl p-1">Cards</h1>
+                    {/* Variants */}
+                    <div className="grid grid-rows-2 grid-cols-3 border border-gray-300">
+
+                        {/* Header */}
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Primary
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Secondary
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            Accent
+                        </div>
+
+                        {/* 1 row */}
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Card variant="primary" size="md">
+                                <p>Test Card<br />Test Card<br />Test Card<br />Test Card</p>
+                            </Card>
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Card variant="secondary" size="md">
+                                <p>Test Card<br />Test Card<br />Test Card<br />Test Card</p>
+                            </Card>
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Card variant="accent" size="md">
+                                <p>Test Card<br />Test Card<br />Test Card<br />Test Card</p>
+                            </Card>
+                        </div>
                     </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button variant="accent" size="md" disabled={true}>
-                            LABEL
-                        </Button>
+
+                    {/* Sizes */}
+                    <div className="grid grid-rows-2 grid-cols-3 border border-gray-300">
+                        <div className="grid place-items-center p-3 border border-gray-300">
+                            Small
+                        </div>
+                        <div className="grid place-items-center p-3 border border-gray-300">
+                            Medium
+                        </div>
+                        <div className="grid place-items-center p-3 border border-gray-300">
+                            Large
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Card size="sm">
+                                <p>Test Card<br />Test Card<br />Test Card<br />Test Card</p>
+                            </Card>
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Card size="md">
+                                <p>Test Card<br />Test Card<br />Test Card<br />Test Card</p>
+                            </Card>
+                        </div>
+                        <div className="grid place-items-center p-6 border border-gray-300">
+                            <Card size="lg">
+                                <p>Test Card<br />Test Card<br />Test Card<br />Test Card</p>
+                            </Card>
+                        </div>
                     </div>
                 </div>
 
-                {/* Sizes */}
-                <div className="grid grid-rows-2 grid-cols-3 border border-gray-300">
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        Small
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        Medium
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        Large
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button variant="primary" size="sm">
-                            LABEL
-                        </Button>
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button variant="primary" size="md">
-                            LABEL
-                        </Button>
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button variant="primary" size="lg">
-                            LABEL
-                        </Button>
-                    </div>
-                </div>
-
-                {/* Loading */}
-                <div className="grid grid-rows-1 grid-cols-2">
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        Loading button
-                    </div>
-                    <div className="grid place-items-center p-6 border border-gray-300">
-                        <Button
-                            variant="primary"
-                            size="lg"
-                            onClick={async () => {
-                                await new Promise(res => setTimeout(res, 2000));
-                                console.log('loader finished');
-                            }}>
-                            LABEL
-                        </Button>
-                    </div>
-                </div>
+                {/* Other components */}
+                <h1 className="text-2xl p-1">Other components</h1>
+                <WordCarousel />
             </div>
         </section >
     )
--- apps/frontend/src/features/menu/menu.tsx
+++ apps/frontend/src/features/menu/menu.tsx
@@ -1,7 +1,73 @@
 import Button from "@/components/ui/buttons/Button"
+import Link from "next/link";
+import Image from "next/image";
+import { WordCarousel } from "./components/wordCarousel";
 
 export const Menu = () => {
     return (
-        <section className="min-h-screen h-full">Menu</section>
-    )
+        <section
+            className="h-full flex flex-col"
+        >
+            {/* Background */}
+            <div
+                className="absolute inset-0 bg-cover bg-center z-[-1]"
+                style={{ backgroundImage: `url('./background_worldmap.png')` }}
+            />
+            <div className="absolute inset-0 bg-black/50 z-[-1]" />
+
+            {/* Header */}
+            <header className="h-20 md:h-24
+                        flex flex-row items-center justify-between
+                        bg-transparent p-1 z-10">
+
+                {/* Logo */}
+                <div className="flex items-center h-full">
+                    <Link href="/" className="h-full transition-transform duration-200 ease-in-out transform hover:scale-105 active:scale-95">
+                        <img
+                            src={'./logo.svg'}
+                            alt="Logo"
+                            className="h-full w-auto"
+                        />
+                    </Link>
+                </div>
+
+                {/* Connection buttons */}
+                <div className="flex flex-row items-center justify-between gap-2 md:gap-3 h-full">
+                    <Button variant="secondary">
+                        CONNEXION
+                    </Button>
+                    <Button variant="primary">
+                        INSCRIPTION
+                    </Button>
+                </div>
+            </header>
+
+            {/* Content */}
+            <div className="flex-1 flex flex-col items-center justify-center
+                            pb-24 gap-8 md:gap-6 p-1"
+            >
+                <Image src={'/logo_white.webp'} alt={"logo"} width={112} height={112}></Image>
+
+                <Button variant='primary' className="py-4 md:py-4 lg:py-4">
+                    JOUE SANS COMPTE
+                </Button>
+
+                <div className="flex flex-col gap-3 items-center">
+                    <p className="text-center text-shadow-lg font-bold
+                            text-sm md:text-base
+                            w-[90%] md:w-[60%] lg-[50%]">
+                        Cityborn, c'est le jeu quiz géo-culture où tu découvres où sont nés les célebrités en t'amusant, seul ou avec tes potes&nbsp;!
+                    </p>
+
+                    <div className="flex flex-col gap-0 items-center">
+                        <p className="text-center text-shadow-lg text-shadow-black/40 text-heading font-bold
+                            text-sm md:text-xl">
+                            TROUVE LE LIEU DE NAISSANCE&nbsp;DE
+                        </p>
+                        <WordCarousel />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
