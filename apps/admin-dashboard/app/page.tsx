4a34009c26bfdf895929734057c177233557816e
tpicaud <tom.picaud@hotmail.com>
2025-10-20 13:39:10 +0200

    create admin dashboard

---
+++ apps/admin-dashboard/app/page.tsx
@@ -0,0 +1,12 @@
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getSession();
+
+  if (session?.isAuthenticated) {
+    redirect("/dashboard");
+  } else {
+    redirect("/login");
+  }
+}
