d0f47f50e9727c5a77c8c8516785c2e81e073f9f
tpicaud <tom.picaud@hotmail.com>
2025-11-04 10:57:06 +0100

    remove mongoose

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SessionModule } from './session/session.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { SentenceModule } from './sentence/sentence.module';
 import { GuessObjectModule } from './guess-object/guess-object.module';
 import { IdModule } from './id/id.module';
@@ -26,15 +25,6 @@ import { SearchModule } from './search/search.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    }),
-    MongooseModule.forRoot(process.env.MONGODB_GUESS_OBJECTS_URI || 'mongodb://localhost/nest', {
-      connectionName: 'guessObjects'
-    }),
-    MongooseModule.forRoot(process.env.MONGODB_SENTENCES_URI || 'mongodb://localhost/nest', {
-      connectionName: 'sentences'
-    }),
-    MongooseModule.forRoot(process.env.MONGODB_GAMES_URI || 'mongodb://localhost/nest', {
-      connectionName: 'games'
     }),
     SentenceModule,
     SessionModule,
