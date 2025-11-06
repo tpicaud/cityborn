4aec1418f65678022fe012b8fb97b4d3b4b0975a
tpicaud <tom.picaud@hotmail.com>
2025-11-06 10:11:51 +0100

    better init script

--- apps/backend/dev/supabase/init.bat
+++ apps/backend/dev/supabase/init.bat
@@ -1,3 +1,5 @@
 set PGPASSWORD=postgres
+psql -h localhost -p 5432 -U postgres -d postgres -f .\drop_all_prisma_migrations.sql
+psql -h localhost -p 5432 -U postgres -d postgres -f .\drop_all_tables.sql
 psql -h localhost -p 5432 -U postgres -d postgres -f .\schema.sql
 psql -h localhost -p 5432 -U postgres -d postgres -f .\seed.sql
