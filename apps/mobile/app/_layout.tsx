472fc363e47d594eae6f978861c0ecf9105eb0d2
tpicaud <tom.picaud@hotmail.com>
2025-11-24 15:49:57 +0100

    useless hidden status bar

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -76,7 +76,6 @@ export default function RootLayout() {
               }}
             />
           </Stack>
-          <StatusBar hidden />
         </AuthProvider>
       </View>
     </SafeAreaProvider>
