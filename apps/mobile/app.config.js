a79fb2fb39b7de902b9cad4283154b6e6cedd956
tpicaud <tom.picaud@hotmail.com>
2025-12-19 14:47:18 +0100

    add scheme

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -2,6 +2,7 @@ export default {
   expo: {
     name: 'Cityborn',
     slug: 'cityborn',
+    scheme: 'cityborn',
     version: '1.0.0',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
