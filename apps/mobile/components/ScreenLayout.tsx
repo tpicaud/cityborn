c6a99c11bdbaa5cf7228e4ddc903b22897d89452
tpicaud <tom.picaud@hotmail.com>
2026-01-01 16:30:06 +0100

    apply bg on screens

--- apps/mobile/components/ScreenLayout.tsx
+++ apps/mobile/components/ScreenLayout.tsx
@@ -2,5 +2,5 @@ import { ReactNode } from 'react';
 import { View } from './ui/native/NativeComponents';
 
 export default function ScreenLayout({ children }: { children: ReactNode }) {
-  return <View className="flex-1 px-4 pfo-2">{children}</View>;
+  return <View className="flex-1 px-4 pfo-2 bg-background">{children}</View>;
 }
