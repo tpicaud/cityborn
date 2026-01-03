b1da22e472774bea84f8f57486855fb98186cb39
tpicaud <tom.picaud@hotmail.com>
2026-01-03 10:18:19 +0100

    update meta tag google content

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -61,7 +61,7 @@ export default async function RootLayout({
         />
         <meta
           name="google-site-verification"
-          content="gFNaiS_8H0_ADBty1p6PsNfeYmrO2Z9Cf2pZQOj6Pqs"
+          content="Lun34fZ5pzhInPdhYOUYr7664dGnE7f5tPX8eEEIzMg"
         />
       </head>
       <body
