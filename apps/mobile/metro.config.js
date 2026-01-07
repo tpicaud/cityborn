365ab56cfee59a4de5e6f0d20482a9e20894554d
tpicaud <tom.picaud@hotmail.com>
2026-01-07 11:56:22 +0100

    Debug env

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,7 +1,9 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativewind } = require("nativewind/metro");
- 
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativewind } = require('nativewind/metro');
+
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
- 
+console.log(process.env.EXPO_NO_DOTENV);
+console.log(process.env.APP_VARIANT);
+
 module.exports = withNativewind(config);
