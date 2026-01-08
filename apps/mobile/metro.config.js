b4c151792abd57a5c50f50842f7ee74232fc3c15
tpicaud <tom.picaud@hotmail.com>
2026-01-08 13:59:17 +0100

    clear caches during local builds

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -4,4 +4,6 @@ const { withNativewind } = require('nativewind/metro');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.resetCache = true;
+
 module.exports = withNativewind(config);
