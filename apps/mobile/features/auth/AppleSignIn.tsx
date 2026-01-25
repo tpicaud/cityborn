35c82cbc55a49aa4b3e93d929e4011b4c4a696e0
tpicaud <tom.picaud@hotmail.com>
2026-01-25 21:35:13 +0100

    implement front connection

--- apps/mobile/features/auth/AppleSignIn.tsx
+++ apps/mobile/features/auth/AppleSignIn.tsx
@@ -1,9 +1,47 @@
+import { apiClient } from '@/lib/apiClient';
+import { cn } from '@/lib/utils';
 import * as AppleAuthentication from 'expo-apple-authentication';
+import { useState } from 'react';
 import { View } from 'react-native';
 
 export const SignInWithAppleButton = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signIn = async () => {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      });
+      console.log(credential);
+
+      apiClient.signInWithApple(
+        credential.user,
+        credential.email || '',
+        credential.fullName?.familyName || '',
+        credential.fullName?.givenName || '',
+      );
+    } catch (e: any) {
+      if (e.code === 'ERR_REQUEST_CANCELED') {
+        console.log('Apple sign in canceled by user');
+      } else {
+        console.error(e);
+      }
+    }
+  };
+
+  const handlePress = async () => {
+    try {
+      setIsLoading(true);
+      await signIn();
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <View>
+    <View className={cn(isLoading ? 'opacity-50' : 'opacity-100')}>
       <AppleAuthentication.AppleAuthenticationButton
         buttonType={AppleAuthentication.AppleAuthenticationButtonType.CONTINUE}
         buttonStyle={
@@ -11,23 +49,7 @@ export const SignInWithAppleButton = () => {
         }
         cornerRadius={100}
         style={{ width: 190, height: 42 }}
-        onPress={async () => {
-          try {
-            const credential = await AppleAuthentication.signInAsync({
-              requestedScopes: [
-                AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-                AppleAuthentication.AppleAuthenticationScope.EMAIL,
-              ],
-            });
-            // signed in
-          } catch (e: any) {
-            if (e.code === 'ERR_REQUEST_CANCELED') {
-              // handle that the user canceled the sign-in flow
-            } else {
-              // handle other errors
-            }
-          }
-        }}
+        onPress={handlePress}
       />
     </View>
   );
