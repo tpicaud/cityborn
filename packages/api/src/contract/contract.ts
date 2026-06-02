5fa673ee3f80496eb575bba0580d0d269733a6da
tpicaud <tom.picaud@hotmail.com>
2026-06-02 17:20:28 +0200

    rename admin contract files

--- packages/api/src/contract/contract.ts
+++ packages/api/src/contract/contract.ts
@@ -1,10 +1,10 @@
 import { initContract } from '@ts-rest/core';
 import { authContract } from './auth.contract.js';
+import { categoryAdminContract } from './category.admin.contract.js';
 import { categoryContract } from './category.contract.js';
-import { categoryAdminContract } from './category-admin.contract.js';
+import { guessObjectAdminContract } from './guess-object.admin.contract.js';
 import { guessObjectContract } from './guess-object.contract.js';
-import { guessObjectAdminContract } from './guess-object-admin.contract.js';
-import { searchAdminContract } from './search-admin.contract.js';
+import { searchAdminContract } from './search.admin.contract.js';
 import { sentenceContract } from './sentence.contract.js';
 import { sessionContract } from './session.contract.js';
 import { userContract } from './user.contract.js';
