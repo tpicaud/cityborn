b5e9d0236ba9ab355c378f5ce64ab4ee294bac25
tpicaud <tom.picaud@hotmail.com>
2026-06-26 17:37:46 +0200

    use direct for datasource and database url for runtime

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: process.env.DATABASE_URL ?? 'postgresql://localhost:5432/dummy',
+    url: process.env.DIRECT_URL ?? 'postgresql://localhost:5432/dummy',
   },
 });
